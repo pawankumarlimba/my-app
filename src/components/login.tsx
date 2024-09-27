e7482b880e7e85b1130030e044fb3347efc74a62
Pawan Kumar <pawankumarlimba746@gmail.com>
2024-09-28 02:25:29 +0530

    changes in code

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -7,6 +7,7 @@ import { cn } from "@/lib/utils";
 
  
 import axios from 'axios';
+import Link from "next/link";
 
 
 import React, {  useState } from 'react'
@@ -85,6 +86,14 @@ return (
           Sign In &rarr;
           <BottomGradient />
         </button>
+        <Link href={"/admin/login"}>
+        <button
+          className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+          type="submit"
+        >
+         Login as Admin&rarr;
+          <BottomGradient />
+        </button></Link>
  
       </form>
     </div></div>
