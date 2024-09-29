a3eecc5f30bffe4abd0060d6b7720ac97119a87e
Pawan Kumar <pawankumarlimba746@gmail.com>
2024-09-29 13:06:36 +0530

    Revert "fresh_changes"
    This reverts commit 8497d8a9dca7f2358f54548d96ca3483e63279cf.

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: [
-      'images.unsplash.com',
-      'img.freepik.com',
-      'encrypted-tbn0.gstatic.com',
-      'aceternity.com',
-      'res.cloudinary.com',
-    ],
-  },
-  async headers() {
-    return [
-      {
-        source: '/:path*', // Apply headers to all routes (or adjust to specific routes if necessary)
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'no-cache',
-          },
+    images: {
+        domains: [
+          'images.unsplash.com',
+          'img.freepik.com',
+          'encrypted-tbn0.gstatic.com',
+          'aceternity.com',
+          'res.cloudinary.com',
         ],
       },
-    ];
-  },
 };
 
+  
 export default nextConfig;
