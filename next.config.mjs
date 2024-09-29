8497d8a9dca7f2358f54548d96ca3483e63279cf
Pawan Kumar <pawankumarlimba746@gmail.com>
2024-09-29 12:47:33 +0530

    fresh_changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: [
-          'images.unsplash.com',
-          'img.freepik.com',
-          'encrypted-tbn0.gstatic.com',
-          'aceternity.com',
-          'res.cloudinary.com',
+  images: {
+    domains: [
+      'images.unsplash.com',
+      'img.freepik.com',
+      'encrypted-tbn0.gstatic.com',
+      'aceternity.com',
+      'res.cloudinary.com',
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*', // Apply headers to all routes (or adjust to specific routes if necessary)
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-cache',
+          },
         ],
       },
+    ];
+  },
 };
 
-  
 export default nextConfig;
